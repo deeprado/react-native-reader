1c2a9ed2974d0046a31d733518a41b62c99b49bf
deeprado <siversonw@126.com>
2020-04-13 17:06:41 +0800

    xx

--- reader/src/root.js
+++ reader/src/root.js
@@ -6,10 +6,14 @@
 import React, {Component} from 'react';
 import {Provider} from 'react-redux';
 import configureStore from './store/store';
+// import SplashScreen from 'react-native-splash-screen';
 
 import App from './app';
 
 class rootApp extends Component {
+  // componentDidMount() {
+  //   SplashScreen.hide();
+  // }
   render() {
     return (
       <Provider store={configureStore}>
